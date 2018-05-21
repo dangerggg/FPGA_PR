18184775e4e433339469fd10508188ccfbd4a226
dangerggg <780110073@qq.com>
2018-05-21 09:07:04 +0800

    top interface

---
+++ top_player.vhd
@@ -0,0 +1,11 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity top_player is
+	port(
+		clk : in std_logic;
+		
+	);
+end top_player;
