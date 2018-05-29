e70fcf02db1c5de8ae04596c12d72786fe7ce062
dangerggg <780110073@qq.com>
2018-05-29 08:59:48 +0800

    add file control system, reamained to be done by LYH

---
+++ fileSystem.vhd
@@ -0,0 +1,15 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity fileSystem is
+	port(
+		
+	);
+end fileSystem;
+
+architecture bhv of fileSystem is
+begin
+	
+end bhv;
