f33fd2035c741bf7733087fa26abc016dc9778eb
liyinghui <li-yh16@mails.tsinghua.edu.cn>
2018-06-10 17:10:46 +0800

    complete bin.py to get 8 bit rgb

--- bin.py
+++ bin.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+from PIL import Image
+import numpy as np 
+import matplotlib as plt 
+
 resultname ='result.bin'
-for i in range(1,10000):
-    print(i)
+for i in range(0,3):
+
     if i < 10:
         filename = "0001.哔哩哔哩-小猪佩奇女生版四川话，软绵绵的-45605025[流畅版]000"+str(i)+".bmp"
     elif i < 100:
@@ -10,16 +14,20 @@ for i in range(1,10000):
         filename = "0001.哔哩哔哩-小猪佩奇女生版四川话，软绵绵的-45605025[流畅版]0"+str(i)+".bmp"
     elif i < 10000:
         filename = "0001.哔哩哔哩-小猪佩奇女生版四川话，软绵绵的-45605025[流畅版]"+str(i)+".bmp"
-    with open(filename,'rb') as f:
-        s = f.read()
-        read_data = s[54:921654]
-        if i == 7:
-            with open(resultname,'wb') as f1:
-                f1.write(read_data)
-        else:
-            with open(resultname,'rb') as f0:
-                s0 = f0.read()
-                read_data = s0 + read_data
-            with open(resultname,'wb') as f1:
-                f1.write(read_data)
-
+    
+    nums = 0
+    im = Image.open(filename)
+    img = np.array(im)
+    for rows in range(img.shape[0]):
+        for columns in range(img.shape[1]):
+            x = ((img[rows,columns,0]>>7)&1)*128 + ((img[rows,columns,0]>>6)&1)*64 + ((img[rows,columns,0]>>5)&1)*32 + ((img[rows,columns,1]>>7)&1)*16 + ((img[rows,columns,1]>>6)&1)*8 + ((img[rows,columns,1]>>5)&1)*4 + ((img[rows,columns,2]>>7)&1)*2 + ((img[rows,columns,2]>>6)&1)*1
+            y = x.astype(np.uint8)
+            if nums == 0 and i == 0:
+                with open(resultname,'wb') as f:
+                    f.write(y)
+                nums = nums + 1
+            else:
+                with open(resultname,'ab') as f:
+                    f.write(y)
+                nums = nums + 1
+    print(i)
